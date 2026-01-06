2c7832ac59d77e7bb80c15ee374dfdb56948e77c
chathandevog-hash <chathandevog@gmail.com>
2026-01-06 16:56:26 +0530

    Delete broadcast.py

--- broadcast.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import filters
-from config.config import OWNER_ID
-
-def register_broadcast_handlers(app):
-    @app.on_message(filters.command("broadcast") & filters.private)
-    async def broadcast(_, message):
-        if message.from_user.id != OWNER_ID:
-            return
-        await message.reply("Broadcast placeholder")
