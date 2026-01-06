4fbd65a62fa1c163e9beefdf0f7c71a64e782a92
chathandevog-hash <chathandevog@gmail.com>
2026-01-06 19:47:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,27 @@
 from pyrogram import Client
-from config.config import BOT_TOKEN
+
+from config.config import BOT_TOKEN, API_ID, API_HASH
+
 from handlers.start import register_start_handler
 from handlers.add import register_add_handlers
 from handlers.stop import register_stop_handlers
 from handlers.status import register_status_handlers
 from handlers.broadcast import register_broadcast_handlers
 
-app = Client("AutoMessageBot", bot_token=BOT_TOKEN)
+app = Client(
+    "AutoMessageBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
 
+# Register all handlers
 register_start_handler(app)
 register_add_handlers(app)
 register_stop_handlers(app)
 register_status_handlers(app)
 register_broadcast_handlers(app)
+
+print("🤖 Auto Message Scheduler Bot Started")
 
 app.run()
