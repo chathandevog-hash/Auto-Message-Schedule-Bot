29424572561d60feac659352bd277b89747673ab
chathandevog-hash <chathandevog@gmail.com>
2026-01-06 17:05:09 +0530

    Create admin_check.py

---
+++ utils/admin_check.py
@@ -0,0 +1,5 @@
+from pyrogram.enums import ChatMemberStatus
+
+async def is_admin(client, message):
+    member = await client.get_chat_member(message.chat.id, message.from_user.id)
+    return member.status in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
